31d263ebf4c4b7481c0a4861bf51585b3da33b7d
Promise30 <promiseadeagbo@gmail.com>
2023-03-04 20:10:54 +0100

    Updated StopwatchClass code

---
+++ Stopwatch Project/StopwatchClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch_Project
+{
+    internal class StopwatchClass
+    {
+        private DateTime _startWatch;
+        private DateTime _endWatch;
+        private bool _isRunning;
+
+        public void Start()
+        {
+            
+            if (_isRunning)
+                throw new InvalidOperationException("StopWatch is already running.");
+            _isRunning = true;
+            _startWatch = DateTime.Now;
+        }
+        public void Stop()
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("StopWatch is not running.");
+            _isRunning= false;
+            _endWatch = DateTime.Now;
+        }
+        public TimeSpan GetDuration()
+        { 
+            return _endWatch - _startWatch;
+        }
+    }
+}
